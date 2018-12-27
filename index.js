6164a9caeb2573845894044af93f5a4c35cd7c96
Vernon Thommeret <synotic@gmail.com>
2018-12-26 16:57:31 -0800

    basic arrows + labels

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var WIDTH = 400;
 var HEIGHT = 200;
 
+var PADDING = 20;
+
 var SCALE = 2;
-var ARROW_LEN = 10;
+var ARROW_WIDTH = 4;
+var ARROW_LEN = 7;
+var LINE_CAP = 'round';
+
+var COLOR_MAJOR_SECOND = '#FED031'
+var COLOR_MINOR_THIRD = '#157EFB'
 
 function draw() {
-  var canvas = document.getElementById('scale');
+  var fretboard = document.getElementById('fretboard');
+  var canvas = fretboard.querySelector('canvas');
   var ctx = canvas.getContext('2d');
-	var width = canvas.width;
-	var height = canvas.height;
+  var width = parseInt(fretboard.style.width);
+  var height = parseInt(fretboard.style.height);
 
   // Retina-friendly dimensions
   canvas.width = width * SCALE;
@@ -17,22 +25,57 @@ function draw() {
   canvas.style.height = height;
   ctx.scale(SCALE, SCALE);
 
-	// Canvas config
-  ctx.lineWidth = 2;
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = '#4A78E2';
+  // Padding
+  ctx.translate(PADDING, PADDING);
 
-  arrow(ctx, 20, 20, 200, 80);
+  // Canvas config
+  ctx.lineWidth = 2;
+  ctx.lineCap = LINE_CAP;
+
+  arrow(ctx, 0, 0, 200, 80, COLOR_MINOR_THIRD);
+  arrow(ctx, 0, 120, 100, 90, COLOR_MAJOR_SECOND);
+  arrow(ctx, 300, 0, 300, 100, COLOR_MINOR_THIRD);
 }
 
-function arrow(ctx, x1, y1, x2, y2) {
+function arrow(ctx, x1, y1, x2, y2, color) {
   var dx = x2 - x1;
   var dy = y2 - y1;
   var rot = -Math.atan2(dx, dy);
   var len = Math.sqrt(dx * dx + dy * dy);
 
+  // Save context
+  ctx.save();
+
+  // Colors
+  ctx.fillStyle = color;
+  ctx.strokeStyle = color;
+
+  // Line
+  ctx.translate(x1, y1);
+  ctx.rotate(rot);
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, len - ARROW_LEN);
   ctx.stroke();
+
+  // Head
+  ctx.save();
+  ctx.translate(0, len);
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(-ARROW_WIDTH, -ARROW_LEN);
+  ctx.lineTo(ARROW_WIDTH, -ARROW_LEN);
+  ctx.closePath();
+  ctx.fill();
+  ctx.restore();
+
+  // Label
+  ctx.save();
+  ctx.translate(0, len);
+  ctx.rotate(-rot);
+  ctx.fillText('Hello!', 2, 3);
+  ctx.restore();
+
+  // Restore
+  ctx.restore();
 }
